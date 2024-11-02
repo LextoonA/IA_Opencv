af599e83c62c4fb2e3a6c2e16938dc21d02a78a7
LextoonA <lexcdlm@gmail.com>
2024-11-02 09:20:50 -0500

    Se abordó la libreria Opencv

---
+++ suavizado.py
@@ -0,0 +1,9 @@
+import cv2
+# Cargar la imagen
+image = cv2.imread('images.jpg')    
+# Aplicar el filtro de Gaussiano para suavizar la imagen
+smoothed = cv2.GaussianBlur(image, (5, 5), 0)
+# Mostrar la imagen suavizada
+cv2.imshow('Suavizada', smoothed)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
